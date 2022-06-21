0bcbe819ef54fe0406b1d2f85a363ec69eeb6f50
adnan400 <95062494+adnan400@users.noreply.github.com>
2022-06-22 01:07:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const switchElement = document.querySelector('.switch');
+
+switchElement.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
